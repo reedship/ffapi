a2651690b762b7a40a3246d093d3daeb54de46a6
Braden Shipley <bradenrshipley@gmail.com>
2019-11-23 10:54:56 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const chalk = require('chalk');
+
+const app = express()
+app.set('port', process.env.PORT || 3001)
+
+app.listen(app.get('port'), () => {
+    console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
+    console.log('  Press CTRL-C to stop\n');
+  });
