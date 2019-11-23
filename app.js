3ceab737041f059a771dbc7e4360b7bb04742844
Braden Shipley <bradenrshipley@gmail.com>
2019-11-23 11:04:04 -0600

    added readme

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const chalk = require('chalk');
 
 const app = express()
 app.set('port', process.env.PORT || 3001)
-
+app.set('view engine', 'ejs')
+app.get('/', (req, res, next) => res.render('readme'))
 app.listen(app.get('port'), () => {
     console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
     console.log('  Press CTRL-C to stop\n');
